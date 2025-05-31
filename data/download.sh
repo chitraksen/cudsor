dfa45ba7c0685adab081716bdc8c21bb209a5ac5
chitraksen <chitraksen@gmail.com>
2025-05-31 12:17:44 +0100

    Data download script

--- data/download.sh
+++ data/download.sh
@@ -3,6 +3,6 @@
 curl --remote-name-all \
     https://raw.githubusercontent.com/chitraksen/mnist-data/main/train-images-idx3-ubyte.gz \
     https://raw.githubusercontent.com/chitraksen/mnist-data/main/train-labels-idx1-ubyte.gz \
-    https://raw.githubusercontent.com/chitraksen/mnist-data/main/t10k-images-idx3-ubyte.gz \
+    https://raw.githubusercontent.com/chitraksen/mnist-data/main/t10k-images-idx3-ubyte.gz  \
     https://raw.githubusercontent.com/chitraksen/mnist-data/main/t10k-labels-idx1-ubyte.gz
 gunzip *-ubyte.gz
