55637b5a4197701efd2026112b39f0da48d276c9
chitraksen <chitraksen@gmail.com>
2025-05-31 12:09:01 +0100

    data download script

--- data/download.sh
+++ data/download.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 curl --remote-name-all \
-    https://raw.githubusercontent.com/fgnt/mnist/master/train-images-idx3-ubyte.gz \
-    https://raw.githubusercontent.com/fgnt/mnist/master/train-labels-idx1-ubyte.gz \
-    https://raw.githubusercontent.com/fgnt/mnist/master/t10k-images-idx3-ubyte.gz \
-    https://raw.githubusercontent.com/fgnt/mnist/master/t10k-labels-idx1-ubyte.gz
+    https://raw.githubusercontent.com/chitraksen/mnist-data/main/train-images-idx3-ubyte.gz \
+    https://raw.githubusercontent.com/chitraksen/mnist-data/main/train-labels-idx1-ubyte.gz \
+    https://raw.githubusercontent.com/chitraksen/mnist-data/main/t10k-images-idx3-ubyte.gz \
+    https://raw.githubusercontent.com/chitraksen/mnist-data/main/t10k-labels-idx1-ubyte.gz
 gunzip *-ubyte.gz
